57a91533031111ebb3ddadf4549cb0ee13f8d0e0
WESLEY PLAZA <69492841+wapdrums@users.noreply.github.com>
2023-04-13 14:40:37 -0300

    Add files via upload

---
+++ aula07.py
@@ -0,0 +1,6 @@
+nome = input('Qual seu nome')
+print('Prazer em te conhecer {:5}!'.format(nome))
+
+n1 = int(input('um valor:'))
+n2 = int(input('outro valor:'))
+print('A soma vale {}'.format(n1+n2))
---
+++ aula07b.py
@@ -0,0 +1,9 @@
+#5 faça um programa que leia um numero inteiro e mostre na tela o seu sucessor e seu antecessor
+#6 crie um algoritmo que leia um numero e mostre o seu dobro, triplo e raiz quadrada
+#7 desenvolva um programa que leia as duas novas de um aluno, calcule e mostre a media
+#8 Escreva um programa que leia um valor em metros e o exiba convertido em centimetros e milimetros
+#9 Faça um programa que leia um numero inteiro qualquer e mostre na tela a sua tabuada
+#10 Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dolares ela pode comprar Considere US$ 1,00 = R$ 3,27
+#11 Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua area e a quantidade de tinta necessaria para pinta-la, sabendo que a cada litro de tinta pinta uma area de 2m2.
+#12 Faça um algoritmo que leia o preço de um produto e mostre seue novo preço com 5% de desconto.
+#13 faça um algoritmo que leia um salario de um funcionario e mostre seu novo salario com 15% de aumento.
